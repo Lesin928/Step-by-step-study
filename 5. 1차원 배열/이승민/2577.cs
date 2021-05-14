26d26ee20f598c84ae36659a5bae61963e908cc3
Lesin928 <seung1084@gmail.com>
2021-05-14 21:54:27 +0900

    이미 푼 문제

---
+++ 5. 1차원 배열/이승민/2577.cs
@@ -0,0 +1,26 @@
+﻿/*using System;
+using System.Text;
+
+namespace 백준1
+{
+    class MainProgram
+    {
+        static void Main(string[] args)
+        {
+           
+            int[] ABC = new int[3];
+
+            for (int i = 0; i < 3; i++)
+            {
+                ABC[i] = Int32.Parse(Console.ReadLine());
+            }
+            string A = Convert.ToString(ABC[0]* ABC[1]* ABC[2]);
+
+            for (int i = 0; i < 10; i++)
+            {
+                string[] words = A.Split(new string[] { Convert.ToString(i) }, StringSplitOptions.None);
+                Console.WriteLine(words.Length - 1);
+            }
+        }
+    }
+}*/
