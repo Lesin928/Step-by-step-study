696abb82f0b5f55fad4d0b49b3768e0b842ab9d5
qnqnqnqn <80696465+qnqnqnqn@users.noreply.github.com>
2021-06-19 20:22:38 +0900

    [1단계/강상현/성공] - 10171

---
+++ 1_Step/강상현/10171.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _000000022222222
+{
+    class Program
+    { 
+        static void Main(string[] args)
+        {
+            Console.WriteLine(@"\    /\
+ )  ( ')
+(  /  )
+ \(__)|");
+        }
+    }
+}
