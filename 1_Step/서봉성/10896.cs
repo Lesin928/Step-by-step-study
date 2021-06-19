e3e42efdd2ac9f276bdfbb212cce22a797031d8d
sbs524 <myura123@naver.com>
2021-06-19 14:52:35 +0900

    [1단계/서봉성/성공] - 10896

---
+++ 1_Step/서봉성/10896.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] s1 = Console.ReadLine().Split(' ');
+            int a = int.Parse(s1[0]);
+            int b = int.Parse(s1[1]);
+            Console.WriteLine(a + b);
+            Console.WriteLine(a - b);
+            Console.WriteLine(a * b);
+            Console.WriteLine(a / b);
+            Console.WriteLine(a % b);
+        }
+    }
+}
