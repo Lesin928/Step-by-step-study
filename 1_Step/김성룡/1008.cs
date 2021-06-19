01b8d5be0234cbf43368d7e2db41966035c2042e
tjdfyd0826 <85820106+tjdfyd0826@users.noreply.github.com>
2021-06-19 20:20:19 +0900

    1

---
+++ 1_Step/김성룡/1008.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] s = ReadLine().Split(' ');
+
+            double a = double.Parse(s[0]);
+            double b = double.Parse(s[1]);
+
+            WriteLine(a / b);
+        }
+    }
+}
+
+
+
