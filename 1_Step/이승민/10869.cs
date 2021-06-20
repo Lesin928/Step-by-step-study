f0ba4361977be3e44d87a7351e87149ae3599e8e
Lesin928 <seung1084@gmail.com>
2021-06-20 17:31:13 +0900

    [1단계/이승민/성공] - 10869

---
+++ 1_Step/이승민/10869.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace 백준1
+{
+    class MainProgram
+    {
+        static void Main(string[] args)
+        {
+            string []a = Console.ReadLine().Split(' ');
+            Console.WriteLine(Convert.ToInt32(a[0]) + Convert.ToInt32(a[1]));
+            Console.WriteLine(Convert.ToInt32(a[0]) - Convert.ToInt32(a[1]));
+            Console.WriteLine(Convert.ToInt32(a[0]) * Convert.ToInt32(a[1]));
+            Console.WriteLine(Convert.ToInt32(a[0]) / Convert.ToInt32(a[1]));
+            Console.WriteLine(Convert.ToInt32(a[0]) % Convert.ToInt32(a[1]));
+
+
+        }
+    }
+}
+
