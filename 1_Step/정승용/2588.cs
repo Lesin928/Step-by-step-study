dbdb9f4bca49d902c5487bfde2a28f1a1e0369a7
Jung Seung Yong <85979224+syong39@users.noreply.github.com>
2021-06-20 18:40:25 +0900

    [1단계/정승용/성공]-2588

---
+++ 1_Step/정승용/2588.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2588
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int  a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            int num1 = b;
+            int num2 = num1;
+
+            while (num2 > 0)
+            {
+                num1 = num2 % 10;
+                Console.WriteLine(a*num1);
+                num2 /= 10;
+            }
+            Console.WriteLine(a * b);
+        }
+    }
+}
