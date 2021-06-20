a1297986c4c3790d13a96e30ba0cb67ec12cf820
Lesin928 <seung1084@gmail.com>
2021-06-20 17:27:11 +0900

    [1단계/이승민/성공] - 1008

---
+++ 1_Step/이승민/1008.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace 백준1
+{
+    class MainProgram
+    {
+        static void Main(string[] args)
+        {
+            string []a = Console.ReadLine().Split(' ');
+            Console.WriteLine(Convert.ToDouble(a[0]) / Convert.ToDouble(a[1]));
+
+
+        }
+    }
+}
+
