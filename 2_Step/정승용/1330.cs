4c71ca4ffe3d46333e462a9a5d5058157931f2c2
Jung Seung Yong <85979224+syong39@users.noreply.github.com>
2021-06-21 01:16:54 +0900

    [2단계/정승용/성공]-1330

---
+++ 2_Step/정승용/1330.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _1330
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string []a = Console.ReadLine().Split();
+            int i = int.Parse(a[0]);
+            int j = int.Parse(a[1]);
+
+            if (i > j)
+            {
+                Console.WriteLine(">");
+            }
+            else if (i < j)
+            {
+                Console.WriteLine("<");
+            }
+            else {
+                Console.WriteLine("==");
+            }
+        }
+    }
+}
