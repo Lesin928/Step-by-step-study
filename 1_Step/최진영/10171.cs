664c34ed4cbaced915a637df0decec25cce2d7ad
SjaegerS <80696106+SjaegerS@users.noreply.github.com>
2021-06-20 15:39:46 +0900

    [1단계/최진영/성공]-10171

---
+++ 1_Step/최진영/10171.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10171
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\\    /\\");
+            Console.WriteLine(" )  ( ')");
+            Console.WriteLine("(  /  )");
+            Console.WriteLine(" \\(__)|");
+        }
+    }
+}
