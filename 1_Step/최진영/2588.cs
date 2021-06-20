e25f66b4f1d1cf24b537ea6b2895e297a4f6c544
SjaegerS <80696106+SjaegerS@users.noreply.github.com>
2021-06-20 18:01:33 +0900

    [1단계/최진영/성공]-2588

---
+++ 1_Step/최진영/2588.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2588
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("{0}", a * (b % 10));
+            Console.WriteLine("{0}", a * ((b / 10)%10));
+            Console.WriteLine("{0}", a * (b / 100));
+            Console.WriteLine("{0}", a * b);
+        }
+    }
+}
