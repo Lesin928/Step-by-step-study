d6e270fe867e2784cafdb822b2813fc917a7b96c
Jung Seung Yong <85979224+syong39@users.noreply.github.com>
2021-06-20 18:35:33 +0900

    [1단계/정승용/성공]-10430

---
+++ 1_Step/정승용/10430.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _10430
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] s = Console.ReadLine().Split();
+            int a = int.Parse(s[0]);
+            int b = int.Parse(s[1]);
+            int c = int.Parse(s[2]);
+
+            Console.WriteLine((a+b)%c);
+            Console.WriteLine((a%c+b%c)%c);
+            Console.WriteLine(a*b%c);
+            Console.WriteLine(((a%c)*(b%c))%c);
+
+
+        }
+    }
+}
