5b761273731ee3603f3f6dc3232c498922a5f7ea
Jung Seung Yong <85979224+syong39@users.noreply.github.com>
2021-06-21 01:34:28 +0900

    [2단계/정승용/성공]-9498

---
+++ 2_Step/정승용/9498.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _9498
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int score = int.Parse(Console.ReadLine());
+            if (score >= 90) Console.WriteLine("A");
+            else if (score >= 80) Console.WriteLine("B");
+            else if (score >= 70) Console.WriteLine("C");
+            else if (score >= 60) Console.WriteLine("D");
+            else Console.WriteLine("F");
+        }
+    }
+}
