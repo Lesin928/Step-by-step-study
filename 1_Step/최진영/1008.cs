a716dc8ce3d585e0cf9684d6a931be8002060169
SjaegerS <80696106+SjaegerS@users.noreply.github.com>
2021-06-20 17:31:18 +0900

    [1단계/최진영/성공]-1008

---
+++ 1_Step/최진영/1008.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string i = Console.ReadLine();
+            string[] m = i.Split();
+            double a = double.Parse(m[0]);
+            double b = double.Parse(m[1]);
+            Console.Write("{0}", a / b);
+        }
+    }
+}
