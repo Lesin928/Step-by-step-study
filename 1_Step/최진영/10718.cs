a36e1f52ce60efffe95147538cdbd00f68a33c78
SjaegerS <80696106+SjaegerS@users.noreply.github.com>
2021-06-20 12:17:31 +0900

    [1단계/최진영/성공]-10718

---
+++ 1_Step/최진영/10718.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10718
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("강한친구 대한육군");
+            Console.WriteLine("강한친구 대한육군");
+        }
+    }
+}
