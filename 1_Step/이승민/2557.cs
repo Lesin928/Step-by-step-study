7bfa666cbbfdcfb106e28f555ad5c24d0bb02b52
Lesin928 <seung1084@gmail.com>
2021-06-20 16:26:13 +0900

    2557 수정

--- 1_Step/이승민/2557.cs
+++ 1_Step/이승민/2557.cs
@@ -1,18 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿/*using System;
 
 namespace 백준1
 {
-    class Program
+    class MainProgram
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
 
-            
         }
     }
 }
+
+*/
