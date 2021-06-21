6cfa8257f0b683b22073b58d2c59875c8c7d5252
qnqnqnqn <80696465+qnqnqnqn@users.noreply.github.com>
2021-06-21 11:20:12 +0900

    [2단계/강상현/성공] - 1330

---
+++ 2_Step/강상현/1330.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _000000022222222
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string c = Console.ReadLine();
+            string[] cc = c.Split();
+
+            int a = int.Parse(cc[0]);
+            int b = int.Parse(cc[1]);
+
+            if (a > b)
+            {
+                Console.WriteLine(">");
+            }
+            else if (a < b)
+            {
+                Console.WriteLine("<");
+            }
+            else
+            {
+                Console.WriteLine("==");
+            }
+        }
+    }
+}
