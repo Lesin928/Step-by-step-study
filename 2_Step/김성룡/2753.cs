a305202342b3c73245423edfbb785158086f9019
tjdfyd0826 <85820106+tjdfyd0826@users.noreply.github.com>
2021-06-21 21:31:26 +0900

    [2단계/김성룡/성공] - 2753

---
+++ 2_Step/김성룡/2753.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] s = ReadLine().Split(' ');
+
+            int a = int.Parse(s[0]);
+
+            if ((a % 4 == 0) && (a % 100 != 0))
+            {
+                WriteLine(1);
+            }
+            else if ((a % 4 == 0) && (a % 400 == 0))
+            {
+                WriteLine(1);
+            }
+            else
+                WriteLine(0);
+        }
+    }
+}
