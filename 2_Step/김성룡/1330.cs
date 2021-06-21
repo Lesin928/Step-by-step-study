314b46f6501da0a17d49b87f13a64e12b55af60a
tjdfyd0826 <85820106+tjdfyd0826@users.noreply.github.com>
2021-06-21 21:31:07 +0900

    [2단계/김성룡/성공] - 1330

---
+++ 2_Step/김성룡/1330.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] s = ReadLine().Split(' ');
+
+            int a = int.Parse(s[0]);
+            int b = int.Parse(s[1]);
+
+            if (a > b)
+            {
+                WriteLine('>');
+            }
+            else if (a < b)
+            {
+                WriteLine('<');
+            }
+            else if (a == b)
+            {
+                WriteLine("==");
+            }
+        }
+    }
+}
