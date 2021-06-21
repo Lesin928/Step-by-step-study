1611c6e5e91a01e1b914bb0cd53d2d36b6e483f9
Jung Seung Yong <85979224+syong39@users.noreply.github.com>
2021-06-22 02:04:37 +0900

    [2단계/정승용/성공] - 2884

---
+++ 2_Step/정승용/2884.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace _2884
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] a = Console.ReadLine().Split();
+            int h = int.Parse(a[0]);
+            int m = int.Parse(a[1]);
+
+            m -= 45;
+
+            if (m < 0) { h -= 1; m += 60; }
+            if (h < 0) h += 24;
+
+            WriteLine(h + " " + m);
+
+
+
+        }
+    }
+}
