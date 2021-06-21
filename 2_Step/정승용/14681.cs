f108bb531aea385e60f2f5129ef31aa2b3aa7e75
Jung Seung Yong <85979224+syong39@users.noreply.github.com>
2021-06-22 01:10:25 +0900

    [2단계/정승용/성공] - 14681

---
+++ 2_Step/정승용/14681.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _14681
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+            int y = int.Parse(Console.ReadLine());
+
+            if (x > 0 && y > 0) Console.WriteLine("1");
+            else if (x < 0 && y > 0) Console.WriteLine("2");
+            else if (x < 0 && y < 0) Console.WriteLine("3");
+            else Console.WriteLine("4");
+
+        }
+    }
+}
