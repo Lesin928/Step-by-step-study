721996749c55ffb1025164be7299a3ea0dd3ebaa
Jung Seung Yong <85979224+syong39@users.noreply.github.com>
2021-06-22 00:59:50 +0900

    [2단계/정승용/성공] - 2753

---
+++ 2_Step/정승용/2753.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+namespace _2753
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] a = Console.ReadLine().Split();
+            int year = int.Parse(a[0]);
+
+            if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) Console.WriteLine("1");
+            else Console.WriteLine("0");
+        }
+    }
+}
