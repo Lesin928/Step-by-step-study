b2ba95a86d909f4c9eac8a6b4362368fae1c75a3
Jung Seung Yong <85979224+syong39@users.noreply.github.com>
2021-06-23 00:49:25 +0900

    [3단계/정승용/성공] - 2739

---
+++ 3_Step/정승용/2739.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2739
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            for (int i = 1; i< 10; i++) {
+                Console.WriteLine("{0} * {1} = {2}", a, i, a * i);
+            }
+        }
+    }
+}
