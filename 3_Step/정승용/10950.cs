c9b2e7d61390c5fe2bf38617d76b0c4a831b1ed3
Jung Seung Yong <85979224+syong39@users.noreply.github.com>
2021-06-23 01:06:05 +0900

    [3단계/정승용/성공] - 10950

---
+++ 3_Step/정승용/10950.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c10950
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int j = int.Parse(Console.ReadLine());
+ 
+            for (int i = 0; i < j; i++)
+            {
+
+                string[] a = Console.ReadLine().Split();
+
+                Console.WriteLine(int.Parse(a[0]) + int.Parse(a[1]));
+            }
+        }
+    }
+}
