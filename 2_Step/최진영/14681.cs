c0685897e413eb16eeb4806213a09183c7c7e3ea
SjaegerS <80696106+SjaegerS@users.noreply.github.com>
2021-06-23 14:23:37 +0900

    [2단계/최진영/성공]-14681

---
+++ 2_Step/최진영/14681.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14681
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            if(a>0)
+            {
+                if(b>0)
+                {
+                    Console.WriteLine("1");
+                }
+                else if(b<0)
+                {
+                    Console.WriteLine("4");
+                }
+            }
+            else if(a<0)
+            {
+                if (b > 0)
+                {
+                    Console.WriteLine("2");
+                }
+                else if (b < 0)
+                {
+                    Console.WriteLine("3");
+                }
+            }
+        }
+    }
+}
