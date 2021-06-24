07a876e94b614cc9eaacf8d89ba9ea2d377c3ebe
SjaegerS <80696106+SjaegerS@users.noreply.github.com>
2021-06-24 21:28:32 +0900

    [3단계/최진영/성공]-2739

---
+++ 3_Step/최진영/2739.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2739
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            for(int b =1;b<10;b++)
+            {
+                Console.WriteLine("{0} * {1} = {2}", a, b, a * b);
+            }
+        }
+    }
+}
