b79abe93328eedb032b8beca28082e753d472608
SjaegerS <80696106+SjaegerS@users.noreply.github.com>
2021-06-24 22:02:43 +0900

    [3단계/최진영/성공]-15552

---
+++ 3_Step/최진영/15552.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15552
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            StringBuilder nums = new StringBuilder();
+            for (int i = 0; i < a; i++)
+            {
+                string s = Console.ReadLine();
+                string[] v = s.Split();
+                int first = int.Parse(v[0]);
+                int second = int.Parse(v[1]);
+                nums.Append(first + second + "\n");
+            }
+            Console.WriteLine(nums.ToString());
+        }
+    }
+}
