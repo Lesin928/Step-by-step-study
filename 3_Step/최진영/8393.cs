09419f54ce8fa355331d046c5193a8089efd6989
SjaegerS <80696106+SjaegerS@users.noreply.github.com>
2021-06-24 21:41:06 +0900

    [3단계/최진영/성공]-8393

---
+++ 3_Step/최진영/8393.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8393
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int b = 0;
+            int a = int.Parse(Console.ReadLine());
+            for(int i=1;i<=a;i++)
+            {
+                b = b + i;
+            }
+            Console.WriteLine("{0}", b);
+        }
+    }
+}
