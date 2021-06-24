4738c748c8f39362b54a5308ff4cb28a43d5a9f2
sbs524 <myura123@naver.com>
2021-06-24 21:28:28 +0900

    [3단계/서봉성/성공] - 2438

---
+++ 3_Step/서봉성/2438.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write('*');
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
