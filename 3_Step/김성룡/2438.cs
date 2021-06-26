b8d02c785abf754511a0f6c637b648621d389b74
tjdfyd0826 <85820106+tjdfyd0826@users.noreply.github.com>
2021-06-26 21:32:57 +0900

    [3단계/김성룡/성공] - 2438

---
+++ 3_Step/김성룡/2438.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(ReadLine());
+
+            for (int i = 0; i < a; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    Write("*");
+                }
+                WriteLine();
+            }
+        }
+    }
+}
