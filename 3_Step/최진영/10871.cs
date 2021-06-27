dbc090811f9fc9c33c86f6955ac37f15d4ad87c4
SjaegerS <80696106+SjaegerS@users.noreply.github.com>
2021-06-27 22:47:21 +0900

    [3단계/최진영/성공]-10871

---
+++ 3_Step/최진영/10871.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10871
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, a;
+            string goal = Console.ReadLine();
+            string[] gg = goal.Split();
+            n = int.Parse(gg[0]);
+            a = int.Parse(gg[1]);
+            string num = Console.ReadLine();
+            string[] nums = num.Split();
+            StringBuilder alpha = new StringBuilder();
+            for(int i=0; i<n;i++)
+            {
+                if(int.Parse(nums[i]) < a)
+                {
+                    alpha.Append(nums[i]+" ");
+                }
+            }
+            Console.WriteLine(alpha.ToString());
+        }
+    }
+}
