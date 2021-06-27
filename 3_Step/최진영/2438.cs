7f62337365fbfa67d04a6ceced513bba62459fa1
SjaegerS <80696106+SjaegerS@users.noreply.github.com>
2021-06-27 16:10:28 +0900

    [3단계/최진영/성공]-2438

---
+++ 3_Step/최진영/2438.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2438
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            StringBuilder star = new StringBuilder();
+            for(int i=1;i<=a;i++)
+            {
+                for(int j=1;j<=a;j++)
+                {
+                    if(j>i)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        star.Append("*");
+                    }
+                }
+                star.Append("\n");
+            }
+            Console.WriteLine(star.ToString());
+        }
+    }
+}
