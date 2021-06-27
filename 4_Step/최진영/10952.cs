281f04bb38a59815d279dccd8ffcccd7610eefd3
SjaegerS <80696106+SjaegerS@users.noreply.github.com>
2021-06-27 23:03:59 +0900

    [4단계/최진영/성공]-10952

---
+++ 4_Step/최진영/10952.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10952
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            StringBuilder answer = new StringBuilder();
+            while (i > -1 )
+            {
+                string s = Console.ReadLine();
+                string[] v = s.Split();
+                int first = int.Parse(v[0]);
+                int second = int.Parse(v[1]);
+                if(first ==0 && second == 0)
+                {
+                    break;
+                }
+                answer.Append(first + second + "\n");
+                i++;
+            }
+            Console.WriteLine(answer.ToString());
+        }
+    }
+}
