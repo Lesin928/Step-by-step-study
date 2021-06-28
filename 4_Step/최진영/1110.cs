ac7702866e425ff0d56228d0651fb456a41ebdc4
SjaegerS <80696106+SjaegerS@users.noreply.github.com>
2021-06-28 11:21:51 +0900

    [4단계/최진영/성공]-1110

---
+++ 4_Step/최진영/1110.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1110
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b;
+            int c;
+            int d=0;
+            int i = 0;
+            if (a <= 9)
+            {
+                a = a * 10;
+            }
+            int e = a;
+            while (true)
+            {
+                b = a / 10;
+                c = a % 10;
+                d = b + c;
+                if (d > 9)
+                {
+                    d = d % 10;
+                }
+                a = (c * 10) + d;
+                i++;
+                if (e == a)
+                {
+                    Console.WriteLine("{0}", i);
+                    break;
+                }
+            }
+        }
+    }
+}
