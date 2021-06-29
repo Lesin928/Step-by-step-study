123ab4c36282d15d819630f1fdd0086e661a3652
Jung Seung Yong <85979224+syong39@users.noreply.github.com>
2021-06-29 17:16:36 +0900

    [4단계/정승용/성공]-1110

---
+++ 4_Step/정승용/1110.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = 0;
+            int count = 0;
+
+            while (b!= a) {
+                if (b == 0){ b = a; }
+                count++;
+                b = 10 * (b % 10) + ((b / 10) + (b % 10)) % 10;
+            }
+            if (b == 0) {
+                Console.WriteLine(1);
+            } else { 
+                Console.WriteLine(count); 
+            }
+        }
+    }
+}
