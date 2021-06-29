a9294bfd8bcde7a91412e8bf4a7d345f5a238c91
tjdfyd0826 <85820106+tjdfyd0826@users.noreply.github.com>
2021-06-29 20:33:31 +0900

    [4단계/김성룡/성공]-10951

---
+++ 4_Step/김성룡/10951.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                string s1 = ReadLine();
+                if (s1 == null)
+                {
+                    break;
+                }
+                string[] s = s1.Split(' ');
+                int a = int.Parse(s[0]);
+                int b = int.Parse(s[1]);
+                int c = a + b;
+
+                WriteLine(c);
+            }
+        }
+    }
+}
