4e270dec3e5138c683710a277e92da35a8faa3db
tjdfyd0826 <85820106+tjdfyd0826@users.noreply.github.com>
2021-06-29 20:33:36 +0900

    [4단계/김성룡/성공]-1110

---
+++ 4_Step/김성룡/1110.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(ReadLine());
+            int result = a;
+            int cnt = 0;
+            while(a!=result)
+            {
+                cnt++;
+                int b = a / 10;
+                int c = a % 10;
+                a = (c * 10) + ((b + c)%10);
+            }
+            WriteLine(cnt);
+        }
+    }
+}
