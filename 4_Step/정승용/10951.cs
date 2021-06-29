03bb8748b2b23ddcdaab952e55afa9ed7ffcc4fd
Jung Seung Yong <85979224+syong39@users.noreply.github.com>
2021-06-29 17:16:18 +0900

    [4단계/정승용/성공]-10951

---
+++ 4_Step/정승용/10951.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true) {
+
+                string st = Console.ReadLine();
+                if (st == null)
+                {
+                    break;
+                }
+                string[] num = st.Split();
+                int a = int.Parse(num[0]);
+                int b = int.Parse(num[1]);
+                Console.WriteLine(a+b);
+            }
+		}
+    }
+}
