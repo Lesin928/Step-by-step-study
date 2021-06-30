3678a4248389b53d67df870849591000026e7346
SjaegerS <80696106+SjaegerS@users.noreply.github.com>
2021-06-30 21:23:08 +0900

    [5단계/최진영/성공]-3052

---
+++ 5_Step/최진영/3052.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3052
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] count = new int[10];
+            int[] rest = new int[42];
+            int divide,result=0;
+            for (int i = 0;i<=9 ; i++)
+            {
+                count[i] = int.Parse(Console.ReadLine());
+            }
+            for(int j=0;j<=9;j++)
+            {
+                divide = count[j] % 42;
+                rest[divide] = rest[divide] + 1;
+            }
+            for(int k=0;k<42;k++)
+            {
+                if(rest[k]>0)
+                {
+                    result = result + 1;
+                }
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
