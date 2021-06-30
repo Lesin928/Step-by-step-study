bbd7fd76239c316f1f40d8bd434419a712e174be
dntjd <jjok1021@naver.com>
2021-07-01 01:06:03 +0900

    [3단계/손우성/성공] - 2739, 10950

---
+++ 3_Step/손우성/2739.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _3단계
+{
+    class _2739
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+
+            for (int i = 1; i < 10; i++)
+            {
+                Console.WriteLine(s + " * " + i + " = " + int.Parse(s) * i);
+            }
+        }
+    }
+}
