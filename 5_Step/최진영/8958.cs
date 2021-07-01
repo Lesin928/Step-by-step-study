aaf5db2757d2d46d77836a4859bcbcb70cb7d5c3
SjaegerS <80696106+SjaegerS@users.noreply.github.com>
2021-07-01 14:57:55 +0900

    [5단계/최진영/성공]-8958

---
+++ 5_Step/최진영/8958.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8958
+{
+    class Program
+    {
+		static void Main(string[] args)
+		{
+			int point = 0;
+			int a = int.Parse(Console.ReadLine());
+			int[] score = new int[a];
+			int v = 0;
+			string[] b = new string[a];
+			for (int j = 0; j < a; j++)
+			{
+				b[j] = Console.ReadLine();
+			}
+			for (int i = 0; i < a; i++)
+			{
+				char[] bb = b[i].ToCharArray();
+				while(true)
+				{
+					if (bb[v] == 'O')
+					{
+						point++;
+						score[i] = score[i] + point;
+					}
+					else
+					{
+						point = 0;
+					}
+					v++;
+					if(v==bb.Length)
+                    {
+						point = 0;
+						v = 0;
+						break;
+                    }
+				}
+			}
+			for (int k = 0; k < a; k++)
+			{
+				Console.WriteLine(score[k]);
+			}
+		}
+    }
+}
