1c70d619292a45dfac2beeefecbd8d3934ad1fb7
Jung Seung Yong <85979224+syong39@users.noreply.github.com>
2021-07-02 15:08:46 +0900

    [5단계/정승용/성공]-2577

---
+++ 5_Step/정승용/2577.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] arr = new int[10];
+
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+
+            int n = a * b * c;
+            int num;
+
+            while (n > 0) {
+                num = n % 10;
+                arr[num]++;
+                n /= 10;
+            }
+
+
+            for (int i = 0; i < 10; i++) {
+                Console.WriteLine(arr[i]);
+
+            }
+
+        }
+    }
+}
