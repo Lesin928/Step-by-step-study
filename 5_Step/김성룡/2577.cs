5f302f29fcd3976a2e8dfd6642ee50285625683d
tjdfyd0826 <85820106+tjdfyd0826@users.noreply.github.com>
2021-07-02 21:53:43 +0900

    [5단계/김성룡/성공]-2577

---
+++ 5_Step/김성룡/2577.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace ConsoleApp1
+{
+    class Class1
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(ReadLine());
+            int b = int.Parse(ReadLine());
+            int c = int.Parse(ReadLine());
+            int[] d = new int[10];
+            int e = a * b * c;
+            while (true)
+            {
+                if (e == 0)
+                    break;
+                int num = e % 10;
+                d[num]++;
+                e /= 10;
+            }
+            for (int i = 0; i < d.Length; i++)
+            {
+                WriteLine(d[i]);
+            }
+        }
+    }
+}
