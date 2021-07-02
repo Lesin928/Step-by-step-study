ef364b24d101b3a4cad18288bb7b80ccda7912d5
SjaegerS <80696106+SjaegerS@users.noreply.github.com>
2021-07-02 17:24:13 +0900

    [6단계/최진영/성공]-1065

---
+++ 6_Step/최진영/1065.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1065
+{
+    class Program
+    {
+		static void Main(string[] args)
+		{
+			int count;
+			int goal = int.Parse(Console.ReadLine());
+			count = use(goal);
+			Console.WriteLine(count);
+		}
+		static int use(int last)
+		{
+			int num = 1;
+			for(int i=2;i<=last;i++)
+            {
+				int hund, ten, one;
+				hund = i / 100;
+				ten = (i % 100) / 10;
+				one = (i % 100) % 10;
+				if (i<100)
+                {
+					num++;
+                }
+				else if(hund-ten==ten-one)
+                {
+					num++;
+                }
+            }
+			return num;
+		}
+    }
+}
