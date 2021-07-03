5c8b58fba25597787e09c80852799f04cfc7582d
SjaegerS <80696106+SjaegerS@users.noreply.github.com>
2021-07-03 22:26:48 +0900

    [7단계/최진영/성공]-11720

---
+++ 7_Step/최진영/11720.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11720
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+            string num = Console.ReadLine();
+            int result = 0;
+            for(int i =0;i<num.Length;i++)
+            {
+                result = result + int.Parse(num[i].ToString());
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
