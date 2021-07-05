19b69cd028591b8fdd602da8f9bb9888e41e1445
SjaegerS <80696106+SjaegerS@users.noreply.github.com>
2021-07-05 11:42:37 +0900

    [7단계/최진영/성공]-2908

---
+++ 7_Step/최진영/2908.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2908
+{
+    class Program
+    {
+        static int pare(int[] ber)
+        {
+            int final = 0;
+            int[] usd = new int[2];
+            for(int j=0;j<2;j++)
+            {
+                usd[j] = (ber[j] % 10) * 100 + (ber[j] % 100/10) * 10 + ber[j] / 100;
+            }
+            int first = usd[0];
+            int second = usd[1];
+            if(first>=second)
+            {
+                final = first;
+            }
+            else 
+            {
+                final = second;
+            }
+            return final;
+        }
+        static void Main(string[] args)
+        {
+            string a = Console.ReadLine();
+            string[] hundr = a.Split();
+            int[] num = new int[2];
+            int result = 0;
+            for(int i=0;i<2;i++)
+            {
+                num[i] = Convert.ToInt32(hundr[i]);
+            }
+            result = pare(num);
+            Console.WriteLine(result);
+        }  
+    }
+}
