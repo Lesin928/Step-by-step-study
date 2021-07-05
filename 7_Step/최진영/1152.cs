ecb6ba3adb287dad40dc7b63ff4bf8090a2436f1
SjaegerS <80696106+SjaegerS@users.noreply.github.com>
2021-07-05 10:58:30 +0900

    [7단계/최진영/성공]-1152

---
+++ 7_Step/최진영/1152.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1152
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = Console.ReadLine();
+            string[] aa = a.Split();
+            int num = 0;
+            for(int i=0;i<aa.Length;i++)
+            {
+                num++;
+                if (aa[i] == "") 
+                {
+                    num = num - 1;
+                }
+            }
+            Console.WriteLine(num);
+        }
+    }
+}
