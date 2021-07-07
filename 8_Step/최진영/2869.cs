e9d527c799d39b2a0c9ba372fdf4ad9b4d396da6
SjaegerS <80696106+SjaegerS@users.noreply.github.com>
2021-07-07 12:04:36 +0900

    [8단계/최진영/성공]-2869

---
+++ 8_Step/최진영/2869.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2869
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = Console.ReadLine();
+            string[] aa = a.Split();
+            int speed = 0, fall = 0, tall = 0;
+            int result=0;
+            speed = int.Parse(aa[0]);
+            fall = int.Parse(aa[1]);
+            tall = int.Parse(aa[2]);
+            result = (tall - fall - 1) / (speed - fall) + 1;
+            Console.WriteLine(result);
+        }
+    }
+}
