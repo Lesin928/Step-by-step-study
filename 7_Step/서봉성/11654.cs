aefc051e14d8ce686cc05fc1c16fb8c944fb2d34
sbs524 <myura123@naver.com>
2021-07-11 16:52:04 +0900

    [7단계/서봉성/성공] - 11654

---
+++ 7_Step/서봉성/11654.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            int a = Convert.ToInt32(s[0]);
+            Console.Write(a);
+        }
+    }
+}
