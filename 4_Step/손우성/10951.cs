5936263eb641b151fc0a9735849cf2c2188f170b
dntjd <jjok1021@naver.com>
2021-07-13 00:17:35 +0900

    [4단계/손우성/성공] - 10952 외 2문제

---
+++ 4_Step/손우성/10951.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using static System.Console;
+using System.Text;
+
+namespace _4단계
+{
+    class _10951
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == null) break;
+                string[] inputNum = input.Split();
+                int numA = int.Parse(inputNum[0]);
+                int numB = int.Parse(inputNum[1]);
+                int addNum = numA + numB;
+                WriteLine(addNum);
+            }
+        }
+    }
+}
---
+++ 4_Step/손우성/1110.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using static System.Console;
+using System.Text;
+
+namespace _4단계
+{
+    class _1110
+    {
+        static void Main()
+        {
+            string inputNum = Console.ReadLine();
+
+            int count = 0;
+            int addNum;
+
+            if (10 > int.Parse(inputNum))
+            {
+                inputNum = inputNum + 0;
+            }
+            string originalNum = inputNum;
+
+            while (true)
+            {
+                count++;
+                int a = int.Parse(inputNum) / 10;
+                int b = int.Parse(inputNum) % 10;
+                addNum = a + b;
+
+                inputNum = b.ToString() + (addNum % 10).ToString();
+
+
+                if (int.Parse(inputNum) == int.Parse(originalNum)) break;
+            }
+            WriteLine(count);
+        }
+    }
+}
