a0ca2ee46d4c2e9eef50ca4f58bcc2807302d6af
SjaegerS <80696106+SjaegerS@users.noreply.github.com>
2021-07-20 17:19:10 +0900

    [9단계/최진영/실패]-1929

---
+++ 9_Step/최진영/1929.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1929
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nums = Console.ReadLine();
+            string[] num = nums.Split();
+            int low = int.Parse(num[0]);
+            int high = int.Parse(num[1]);
+            int number = high-low;
+            int[] a = new int[number+1];
+            int k =0;
+            for (int i = low; i <= high; i++)
+            {
+                a[k] = i;
+                k++;
+            }
+
+
+            for (k = 2; k <= number; k++)
+            {
+                for (int j = 2 * k; j <= high; j += k)
+                {
+                    for (int l = 0; l < number; l++)
+                    {
+                        
+                        if (a[l] == 0)
+                        {
+                            Console.WriteLine(j);
+                            continue;
+                        }
+                        else if (a[l] % j == 0)
+                        {
+                            a[l] = 0;
+                        }
+
+                    }
+
+                }
+            }
+            for (int i = 0; i < number; i++)
+            {
+                if (a[i] != 0)
+                {
+                    Console.WriteLine(a[i]);
+                }
+            }
+
+        }
+    }
+}
