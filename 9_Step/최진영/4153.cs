eab2257f6538ec62fb5e28c45f25968b2a502e46
SjaegerS <80696106+SjaegerS@users.noreply.github.com>
2021-07-22 23:17:47 +0900

    [9단계/최진영/성공]-4153

---
+++ 9_Step/최진영/4153.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4153
+{
+    class Program
+    {
+        static void raise(int a, int b, int c)
+        {
+                if(Math.Pow(a,2)== Math.Pow(b, 2) + Math.Pow(c, 2))
+                {
+                Console.WriteLine("right");
+                }
+                else if (Math.Pow(b, 2) == Math.Pow(a, 2) + Math.Pow(c, 2))
+                {
+                Console.WriteLine("right");
+                }
+                else if (Math.Pow(c, 2) == Math.Pow(b, 2) + Math.Pow(a, 2))
+                {
+                Console.WriteLine("right");
+                }
+                else
+                {
+                    Console.WriteLine("wrong");
+                }
+        
+        }
+        static void Main(string[] args)
+        {
+            for(int i=0; ;i++)
+            {
+                string a = Console.ReadLine();
+                string[] aa = a.Split();
+                int fst = int.Parse(aa[0]);
+                int scd = int.Parse(aa[1]);
+                int trd = int.Parse(aa[2]);
+                if (fst == 0 && scd == 0 && trd == 0)
+                {
+                    break;
+                }
+                raise(fst, scd, trd);
+            }
+        }
+    }
+}
