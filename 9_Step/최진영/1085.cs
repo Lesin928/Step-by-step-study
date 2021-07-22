d9444d714a9613807deabce365163b0447474960
SjaegerS <80696106+SjaegerS@users.noreply.github.com>
2021-07-22 13:19:37 +0900

    [9단계/최진영/성공]-1085

---
+++ 9_Step/최진영/1085.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1085
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = Console.ReadLine();
+            string[] so = a.Split();
+            int x, y, w, h;
+            x = int.Parse(so[0]);
+            y = int.Parse(so[1]);
+            w= int.Parse(so[2]);
+            h = int.Parse(so[3]);
+            int right= w - x;
+            int left = x;
+            int up = h- y;
+            int down = y;
+            int range, height;
+            range = right > left ? left : right;
+            height = up > down ? down : up;
+            if (range<height)
+            {
+                Console.WriteLine(range);
+            }
+            else if(range>=height)
+            {
+                Console.WriteLine(height);
+            }
+        }
+    }
+}
