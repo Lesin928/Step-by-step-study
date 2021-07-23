b914be903849d378442ca36309a5c52e60023eae
SjaegerS <80696106+SjaegerS@users.noreply.github.com>
2021-07-23 13:57:06 +0900

    [9단계/최진영/성공]-3053

---
+++ 9_Step/최진영/3053.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3053
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            double uclid = Math.PI * (double)Math.Pow(a, 2);
+            double taxi = 2 * (double)Math.Pow(a, 2);
+            Console.WriteLine("{0:f6}",uclid);
+            Console.WriteLine("{0:f6}", taxi);
+        }
+    }
+}
