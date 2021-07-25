f15708963522982993feb6a3a8f280e4b841b2f4
SjaegerS <80696106+SjaegerS@users.noreply.github.com>
2021-07-25 11:13:27 +0900

    [10단계/최진영/성공]-10872

---
+++ 10_Step/최진영/10872.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10872
+{
+    class Program
+    {
+        static int pac(int a)
+        {
+            if(a<=1)
+            {
+                return 1;
+            }
+            else
+            {
+                return a * pac(a - 1);
+            }
+        }
+        static void Main(string[] args)
+        {
+            int goal = int.Parse(Console.ReadLine());
+            int result = pac(goal);
+            Console.WriteLine(result);
+        }
+    }
+}
