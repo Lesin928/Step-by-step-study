1a2e642963e90a27a3d281c47fe4cc6fc73efb91
SjaegerS <80696106+SjaegerS@users.noreply.github.com>
2021-07-28 01:37:48 +0900

    [10단계/최진영/성공]-11729

---
+++ 10_Step/최진영/11729.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11729
+{
+	
+	class Program
+	{
+		static StringBuilder result = new StringBuilder();
+		static void Main(string[] args)
+		{
+			int size = int.Parse(Console.ReadLine());
+			Console.WriteLine(Math.Pow(2, size) - 1);
+			move(size, 1, 2, 3);
+			Console.WriteLine(result);
+		}
+		static void move(int a, int A, int B, int C)
+		{
+			
+			if (a == 0)
+			{
+				return;
+			}
+			else
+			{
+				move(a - 1, A, C, B);
+				result.Append(A + " " + C + "\n");
+				move(a - 1, B, A, C);
+			}
+		}
+	}
+}
+
